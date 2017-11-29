f478df0ce0105b2eac27817fe0586eb462684a40
Bryan Hyland <bryan.hyland32@gmail.com>
2017-11-28 18:01:15 -0800

    Added Vector Template

--- Containers/main.cpp
+++ Containers/main.cpp
@@ -1,4 +1,5 @@
 #include "IntVector.h"
+#include "TVector.h"
 #include <iostream>
 
 using std::cout;
@@ -23,6 +24,7 @@ bool AssertedBool(bool arg)
 
 int main()
 {
+	/*
 	// Closed Activities
 
 	IntVector vector;
@@ -81,6 +83,67 @@ int main()
 	vector.Append(34);
 	vector.Compact();
 	AssertedEqual(vector.Capacity(), vector.Size());
-	
+	*/
+
+	TVector<float>vector;
+
+	AssertedBool<true>(vector.Empty());
+
+	AssertedEqual<size_t>(vector.Capacity(), 2);
+
+	vector.Append(1);
+	vector.Append(5);
+
+	AssertedEqual<size_t>(vector.Capacity(), 2);
+
+	AssertedEqual<float>(vector.At(0), 1.f);
+	AssertedEqual<float>(vector.At(1), 5.f);
+
+	vector.Append(33.f);
+
+	AssertedEqual<float>(vector.At(0), 1.f);
+	AssertedEqual<float>(vector.At(1), 5.f);
+	AssertedEqual<float>(vector.At(2), 33.f);
+	AssertedEqual<size_t>(vector.Capacity(), 4);
+	AssertedEqual<size_t>(vector.Size(), 3);
+	AssertedEqual<float>(vector.Front(), 1.f);
+	AssertedEqual<float>(vector.Back(), 33.f);
+	AssertedEqual(vector.C_Ptr(), &vector.At(0));
+
+	AssertedEqual<float>(vector[0], 1.f);
+	vector.Clear();
+	AssertedBool<true>(vector.Size() == 0);
+	AssertedBool<true>(vector.Capacity() == 2);
+	AssertedBool<true>(vector.Empty());
+
+	vector.Append(12.f);
+	vector.Append(45.f);
+	vector.Append(12.f);
+
+	AssertedEqual<size_t>(vector.Size(), 3);
+	AssertedEqual(vector.Count(12.f), 2);
+
+	vector.Erase(0);
+	AssertedEqual<float>(vector.Front(), 45.f);
+	vector.Append(89.f);
+	vector.Insert(78.f, 3);
+	AssertedEqual<size_t>(vector[2], 78.f);
+
+	vector.Clear();
+	vector.Reserve(10);
+	AssertedBool<true>(vector.Capacity() >= 10);
+	vector.Append(56.f);
+	vector.Append(90.f);
+	vector.Append(34.f);
+	vector.Compact();
+	AssertedEqual(vector.Capacity(), vector.Size());
+	vector.Pop();
+	AssertedEqual<float>(vector.Back(), 90.f);
+	vector.Append(12.f);
+	vector.Append(78.2f);
+	vector.Append(32.f);
+	vector.EraseRange(1, 3);
+	AssertedEqual<float>(vector.Size(), 2);
+
 	return 0;
 }
